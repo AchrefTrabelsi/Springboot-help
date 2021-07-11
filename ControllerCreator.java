2704c25e3ad4282a67b6aef639c388f759bf0e4f
AchrefTrabelsi <62256755+AchrefTrabelsi@users.noreply.github.com>
2021-07-11 15:02:27 +0100

    Create ControllerCreator.java

---
+++ ControllerCreator.java
@@ -0,0 +1,36 @@
+public class ControllerCreator{
+
+     public static void main(String []args){
+         String[] classes = {"UserAlert","UserAlertTemplate","AlertConfiguration",
+             "AlertConfigurationTemplate","Mesure","Operator"
+         } ;
+         for (String s : classes){
+             String ss=s+"s";
+             String ls=s.substring(0, 1).toLowerCase() + s.substring(1);
+             String lss=ls+"s";
+             System.out.println("@Autowired\nprivate "+s+"Service "+ls+"Service;\n");
+             System.out.println("@GetMapping\n"
+                +"public List<"+s+"> getAll"+s+"() {\n"
+                +"return "+ls+"Service.getAll"+ss+"();\n}\n");
+		        
+         
+             System.out.println("@GetMapping(\"/{id}\")\n"
+            +"public "+s+" get"+s+"(@PathVariable int id){\n"
+		    +"return "+ls+"Service.get"+s+"(id);\n}\n");
+		    
+            System.out.println("@PostMapping\n"
+            +"public void add"+s+"(@RequestBody "+s+" "+ls+") {\n"
+		    +ls+"Service.add"+s+"("+ls+");\n}\n");
+		    
+		    System.out.println("@PutMapping\n"
+		    +"public void update"+s+"(@RequestBody "+s+" "+ls+") {\n"
+		    +ls+"Service.update"+s+"("+ls+");\n}\n");
+		    
+		    System.out.println("@DeleteMapping(\"/{id}\")\n"
+            +"public void delete"+s+"(@PathVariable int id) {\n"
+    		+ls+"Service.delete"+s+"(id);\n}\n");
+		    System.out.println("------------------------------------------------");
+		    //end
+         }
+     }
+}
